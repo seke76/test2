ee157bf03cb10fe755e8cb292da1d0e544f7f150
seklund <seklund@SE07334.opentext.net>
2015-03-16 22:43:56 +0100

    Funkar inte

--- Kundkort/src/seklund/Main.java
+++ Kundkort/src/seklund/Main.java
@@ -5,6 +5,10 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -22,15 +26,20 @@ public class Main extends Application{
 
 		// Layout
 		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.CENTER);
+		pane.setAlignment(Pos.TOP_CENTER);
 		pane.setHgap(10);
 		pane.setVgap(10);
 		pane.setPadding(new Insets(25, 25, 25, 25));
 
+		Text headerLabel = new Text("Kundkort");
+		headerLabel.setFont(Font.font("Arial", FontWeight.NORMAL,35));
+		pane.add(headerLabel, 0, 0, 3, 1);
+		
+		HBox hbox = new HBox();
 		
 		
 		
-		Scene scene = new Scene(pane, 300, 300);
+		Scene scene = new Scene(pane, 1000, 600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
