94f2fc3cf881508f252d7d6010529de8164b3431
seklund <seklund@SE07334.opentext.net>
2015-03-17 15:29:09 +0100

    end of day, ny layout.fxml men den är inte addad. Syns den i github?

--- Kundkort/src/seklund/Main.java
+++ Kundkort/src/seklund/Main.java
@@ -1,12 +1,14 @@
 package seklund;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
@@ -25,36 +27,10 @@ public class Main extends Application{
 
 		primaryStage.setTitle("Kundkort");
 
-		// Layout
-		GridPane pane = new GridPane();
-		pane.setAlignment(Pos.TOP_CENTER);
-		pane.setHgap(10);
-		pane.setVgap(10);
-		pane.setPadding(new Insets(25, 25, 25, 25));
+		Pane pane = (Pane)FXMLLoader.load(getClass().getResource("layout.fxml"));
+		
+		//Scene myScene = new Scene(myPane);
 
-		Text headerLabel = new Text("Kundkort");
-		headerLabel.setFont(Font.font("Arial", FontWeight.NORMAL,35));
-		pane.add(headerLabel, 0, 0, 3, 1);
-		
-		HBox hbox = new HBox();
-        hbox.setPadding(new Insets(15, 12, 15, 12));
-        hbox.setSpacing(10);   // Gap between nodes
-   
-        
-        Button buttonNew = new Button("Ny kund");
-        buttonNew.setPrefSize(100, 20);
-
-        Button buttonSearch = new Button("Sök");
-        buttonSearch.setPrefSize(100, 20);
-        
-        Button buttonList = new Button("List alla");
-        buttonList.setPrefSize(100, 20);
-        
-        hbox.getChildren().addAll(buttonNew, buttonSearch, buttonList);
-        pane.add(hbox, 0, 1);
-		
-		
-		
 		Scene scene = new Scene(pane, 1000, 600);
 		primaryStage.setScene(scene);
 		primaryStage.show();
